d7904652e484d2a78ee8fab17324e013a005aeca
Alex Hewson <alex@mbird.biz>
2013-06-14 19:36:16 +0100

    script to make into a sieve rule

---
+++ scripts/domains2sieve.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+
+# Turn domains.txt into a sieve rule
+
+import os, sys
+
+f = open(sys.argv[1])
+doms = f.readlines()
+doms = map( lambda s: s.strip(), doms )
+doms = filter( lambda s: not s.startswith('#'), doms )
+doms = filter( lambda s: len(s) > 0, doms )
+doms.sort()
+
+def chunks(l, n):
+    """ Yield successive n-sized chunks from l.
+    """
+    for i in xrange(0, len(l), n):
+        yield l[i:i+n]
+
+
+print 'if address :matches "from" ['
+
+for d in doms[:-1]:
+    print "  \"*@%s\"," % d
+
+d = doms[-1]   # last one needs no trailing comma
+print "  \"*@%s\"" % d
+
+
+print """
+  ] {
+    #setflag "\\Seen";
+    fileinto "Recruiters";
+    stop;
+}
+""".strip()
+
